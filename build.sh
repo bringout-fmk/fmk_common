66a91cf3acc75d05d4f92e112769b5d9752bf8e5
Ernad Husremovic <hernad@bring.out.ba>
2012-03-24 23:58:00 +0100

    build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export BASEDIR=~/github
+
+ITEMS="message  bterm  partnst  security pi  svi proizv event rabat ugov exp_dbf xml fiscal  rn roba lokal rules"
+
+for item in $ITEMS
+do
+
+cd $item
+make clean
+make ..
+
+done
