9fc28f0d2da8d2ba0b46421b7b60b2c7abb7ea18
Ernad Husremovic <hernad@bring.out.ba>
2012-03-25 16:54:50 +0200

    rabat/1g

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 export BASEDIR=~/github
 
-ITEMS="message  bterm  partnst  security pi  svi proizv event rabat ugov exp_dbf xml fiscal  rn roba lokal rules"
+ITEMS="message  bterm  partnst  security pi  svi proizv event rabat/1g ugov exp_dbf xml fiscal  rn roba lokal rules"
 
 for item in $ITEMS
 do
 
 cd $item
-make clean
+#make clean
 make 
 cd ..
 
